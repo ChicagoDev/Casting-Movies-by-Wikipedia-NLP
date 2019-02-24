4ba895c6ffbe1497bc337c5ea1898e45862c6972
Blake Gideon <Blake@Gideon.email>
2019-02-24 13:30:02 -0600

    Created code to simplify creation of models. Also created first matching code in notebook.

--- nlp/util.py
+++ nlp/util.py
@@ -1,5 +1,6 @@
 import numpy
 import math
+from nlp.models import Tfidf, TopicModelSVD
 
 @staticmethod
 def cosine_distance(u, v):
@@ -8,3 +9,16 @@ def cosine_distance(u, v):
     u.v / |u||v|.
     """
     return numpy.dot(u, v) / (math.sqrt(numpy.dot(u, u)) * math.sqrt(numpy.dot(v, v)))
+
+
+
+
+@staticmethod
+def get_SVD_model(people):
+    docs = [p['stemmed_wiki_doc'] for p in people]
+    doc_ids = [p['_id'] for p in people]
+
+    model = Tfidf(docs, doc_ids)
+    topic_model = TopicModelSVD(model.model, model.cv_tfidf)
+
+    return (model, topic_model)
